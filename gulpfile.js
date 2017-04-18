9e35b898286012de182b243bc767352bbe4f5fe4
hina2329 <hina.6637@gmail.com>
2017-04-19 00:30:12 +0500

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+// Dependencies
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    plumber = require('gulp-plumber'),
+    browserSync = require('browser-sync');
+
+
+
+/*
+ ===== TASKS =====
+ */
+
+// Task: SASS Compiling
+gulp.task('sass', function () {
+    gulp.src('src/scss/style.scss')
+        .pipe(plumber())
+        .pipe(sass())
+        .pipe(gulp.dest('build/css/'));
+});
+
+
+// Task: Browser Syncing
+gulp.task('browser-sync', function () {
+    browserSync.init(
+        [
+            'build/css/style.css',
+            'build/js/*js',
+            'build/*.html'
+        ],
+        {
+            server: {
+                baseDir: './build'
+            }
+        }
+    );
+});
+
+
+// Task: Watch
+gulp.task('watch', function(){
+    gulp.watch('src/scss/**/*.scss', ['sass']);
+});
+
+
+// Task: DEFAULT
+gulp.task('default', ['browser-sync', 'watch']);
