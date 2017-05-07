ed85b59591bb98a064160111923c8afc4feb66c9
hina2329 <hina.6637@gmail.com>
2017-05-07 15:30:05 +0500

    Review page

--- build/js/custom.js
+++ build/js/custom.js
@@ -1,9 +1,36 @@
-var tabItems = document.getElementById("tabs").getElementsByTagName("li");
+//var tabItems = document.getElementById("tabs").getElementsByTagName("li");
+//
+//for (i = 0; i < tabItems.length; i++) {
+//    var anchor = tabItems[i].getElementsByTagName("a");
+//    //console.log(anchor[0].getAttribute("href"));
+//    anchor[0].addEventListener("click", function(){
+//        alert(this.getAttribute("href"));
+//    });
+//}
+//
+//
+//Review Rating
 
-for (i = 0; i < tabItems.length; i++) {
-    var anchor = tabItems[i].getElementsByTagName("a");
-    //console.log(anchor[0].getAttribute("href"));
-    anchor[0].addEventListener("click", function(){
-        alert(this.getAttribute("href"));
-    });
+var ratingStars = document.getElementById("rating").getElementsByClassName("stars");
+for (i = 0; i < ratingStars.length; i++) {
+    ratingStars[i].addEventListener("click", activeStars);
 }
+function activeStars() {
+    for (i = 0; i < ratingStars.length; i++) {
+        ratingStars[i].classList.remove("active");
+    }
+    this.classList.add("active");
+    }
+//Review Partner
+
+var partnerRating = document.getElementById("partners").getElementsByClassName("rate");
+for (a = 0; a < partnerRating.length; a++) {
+    partnerRating[a].addEventListener("click", activePartner);
+}
+function activePartner() {
+    for (a = 0; a < partnerRating.length; a++) {
+        partnerRating[a].classList.remove("active");
+    }
+    this.classList.add("active");
+}
+
